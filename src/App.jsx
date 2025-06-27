56a9f4ace71d98a2f208b9311701d39585572183
Pradnya <pradnyanayakawade@gmail.com>
2025-06-27 11:04:53 +0530

    pradnya third commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,33 @@ import CoursesPage from "./pages/CoursesPage";
 import AdmissionsPage from "./pages/AdmissionsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ChatbotComponent from "./components/Chatbot/ChatbotComponent";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DevelpoerInfoPopup";
+import { useState } from "react";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Pradnya Nayakawade"
+          studentPhotoUrl="images/Pradnya.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+
+
+
+
+
+
+
     <Router>
       <div className="main-layout">
         <Header />
@@ -30,6 +54,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </>
   );
 }
 export default App;
